382f747c83a70e543299e5d0d1f5ec99faf912f3
ifAronisGida <ifAronsiGida@github.com>
2019-05-24 14:57:28 +0200

    added content for tabs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,23 +5,62 @@
         v-for="post in posts"
         :key="post.id"
         :title="post.date">
-        <p>{{ post.text }}</p>
+        <one-day :text="post.text"/>
       </b-tab>
     </b-tabs>
   </div>
 </template>
 <script>
+import OneDayTab from '@/components/OneDayTab';
+
 export default {
   name: 'Home',
+  components: {
+    'one-day': OneDayTab,
+  },
   data() {
     return {
       posts: [
-        { id: 1, date: '2019-05-10', text: { question: 'daily question', answer: 'daily answer' } },
-        { id: 2, date: '2019-05-11', text: { question: 'daily question', answer: 'daily answer' } },
-        { id: 3, date: '2019-05-12', text: { question: 'daily question', answer: 'daily answer' } },
-        { id: 4, date: '2019-05-13', text: { question: 'daily question', answer: 'daily answer' } },
-        { id: 5, date: '2019-05-14', text: { question: 'daily question', answer: 'daily answer' } },
-        { id: 6, date: '2019-05-15', text: { question: 'daily question', answer: 'daily answer' } },
+        { id: 1,
+          date: '2019-05-10',
+          text: [
+            { id: 1, question: 'sdfasdfasdf question', answer: 'daily answer' },
+            { id: 2, question: 'daily question', answer: 'daily asdlfkj séodifh aséldk fhaséd hfaosidh fanswer' },
+            { id: 3, question: 'daily question', answer: 'daily answer' },
+            { id: 4, question: 'daily question', answer: 'daily answer' },
+            { id: 5, question: 'daily question', answer: 'daily answer' }] },
+        { id: 2,
+          date: '2019-05-11',
+          text: [
+            { id: 1, question: 'daily asdf', answer: 'daily answer' },
+            { id: 2, question: 'daily question', answer: 'daily answer' },
+            { id: 3, question: 'daily question', answer: 'daily answer' },
+            { id: 4, question: 'daily question', answer: 'daily answer' },
+            { id: 5, question: 'daily question', answer: 'daily answer' }] },
+        { id: 3,
+          date: '2019-05-12',
+          text: [
+            { id: 1, question: 'daily question', answer: 'daily answer' },
+            { id: 2, question: 'daily question', answer: 'daily answer' },
+            { id: 3, question: 'daily question', answer: 'dailyasdfasdf answer' },
+            { id: 4, question: 'daily question', answer: 'daily answer' },
+            { id: 5, question: 'daily question', answer: 'daily answer' }] },
+        { id: 4,
+          date: '2019-05-13',
+          text: [
+            { id: 1, question: 'daily question', answer: 'daily answer' },
+            { id: 2, question: 'daily question', answer: 'daily answer' },
+            { id: 3, question: 'dailyasdfasdf question', answer: 'daily answer' },
+            { id: 4, question: 'daily question', answer: 'daily answer' },
+            { id: 5, question: 'daily quasdfasdfestion', answer: 'daily answer' }] },
+        { id: 5,
+          date: '2019-05-14',
+          text: [
+            { id: 1, question: 'daily question', answer: 'daily answer' },
+            { id: 2, question: 'dailasdfasdfy question', answer: 'daily answer' },
+            { id: 3, question: 'daily question', answer: 'daily answer' },
+            { id: 4, question: 'daily question', answer: 'daily answer' },
+            { id: 5, question: 'daily question', answer: 'dailasdfasdfy answer' }] },
       ],
     };
   },
