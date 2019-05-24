15d4c5a89f1e660f63e68c79facd52bd1e940686
ifAronisGida <ifAronsiGida@github.com>
2019-05-24 10:32:46 +0200

    added navbar

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="homeText">
+    WELCOME TO YOUR NEW LIFE HEHEHE
+  </p>
+</template>
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+<style scoped>
+  .homeText{
+    font-size: 35px;
+    color: #ff5678;
+    text-align: center;
+    position: relative;
+    top:30px;
+    text-shadow: 2px 2px 2px gray;
+  }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-navbar
+    toggleable="md"
+    type="dark"
+    variant="info">
+    <b-navbar-brand :to="{ name: 'Home' }">TakeOff Home</b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item href="#">navitem1</b-nav-item>
+      <b-nav-item href="#">navitem2</b-nav-item>
+      <b-nav-item href="#">navitem3</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style scoped>
+
+</style>
